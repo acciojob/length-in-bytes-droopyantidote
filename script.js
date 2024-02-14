979c46ea67e5ca85bf11568b4bb0ad434a57e45a
droopyantidote <144815313+droopyantidote@users.noreply.github.com>
2024-02-14 12:57:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const byteSize = (str) => {
-  // write your code here
+  // Create a Blob object with the string data
+  const blob = new Blob([str]);
+
+  // Retrieve the size of the Blob in bytes
+  const byteLength = blob.size;
+
+  // Return the size in bytes
+  return byteLength;
 };
 
 // Do not change the code below
